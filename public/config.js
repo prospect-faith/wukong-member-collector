ccbe6e2b5b14d13f2b903cd9e9e7f98d656032bc
zoe <1311851156@qq.com>
2026-06-12 19:21:54 +0800

    Add static Supabase submission page

---
+++ public/config.js
@@ -0,0 +1,4 @@
+window.WUKONG_COLLECTOR_CONFIG = {
+  supabaseUrl: "https://jurbeooxmhuieyrxirhn.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1cmJlb294bWh1aWV5cnhpcmhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyMzUwOTksImV4cCI6MjA5NjgxMTA5OX0.XvrTmSOh2vXs7lpng_7opOwOfqZm1G-9A0KAD-ifCi4"
+};
